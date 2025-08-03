e83d4f43d6754515938c0e53e62d89814e9c57d7
Vlad Solomon <vladsolomon720@gmail.com>
2025-08-03 17:36:10 +0300

    basic fetch

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("hello");
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const LASTFM_API_KEY = process.env.LASTFM_API_KEY;
+const LASTFM_RECENT_URL = `http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=vladsolomon&api_key=${LASTFM_API_KEY}&limit=1&page=1&format=json`;
+
+async function getRecentTrack() {
+    const response = await fetch(LASTFM_RECENT_URL);
+    const data = await response.json();
+    const recentTrack = data.recenttracks.track[0];
+    return {
+        title: recentTrack.name,
+        artist: recentTrack.artist["#text"],
+        url: recentTrack.url,
+        live: recentTrack["@attr"]?.nowplaying === "true" ? true : false,
+    };
+}
+
+const recentTrack = await getRecentTrack();
+console.log(recentTrack);
